4902cb9da8e78fb00335cdaeef37d799f70c5f96
aprilsacil <aprilvsacil@gmail.com>
2018-06-07 18:36:46 +0800

    set relation to lowercase cradle-120

--- src/Schema/events.php
+++ src/Schema/events.php
@@ -49,6 +49,10 @@ $this->on('system-schema-create', function ($request, $response) {
                 return $relation['name'] !== '' ? true : false;
             }
         );
+
+        foreach ($data['relations'] as $key => $relation) {
+            $data['relations'][$key]['name'] = strtolower($relation['name']);
+        }
     }
 
     //----------------------------//
@@ -324,6 +328,10 @@ $this->on('system-schema-update', function ($request, $response) {
                 return $relation['name'] !== '' ? true : false;
             }
         );
+
+        foreach ($data['relations'] as $key => $relation) {
+            $data['relations'][$key]['name'] = strtolower($relation['name']);
+        }
     }
 
     //----------------------------//
@@ -370,13 +378,13 @@ $this->on('system-schema-create-elastic', function ($request, $response) {
     if (!file_exists($modelPath)) {
         return $response->setError(true, 'Model doesn\'t exist.');
     }
-    
+
     //----------------------------//
     // 3. Prepare Data
     //----------------------------//
     // get model data
     $data = include_once ($modelPath);
-    
+
     //----------------------------//
     // 4. Process Data
     //----------------------------//
@@ -397,7 +405,7 @@ $this->on('system-schema-create-elastic', function ($request, $response) {
  * @param Response $response
  */
 $this->on('system-schema-search-elastic', function ($request, $response) {
-    
+
     //----------------------------//
     // 1. Get Data
     $data = [];
@@ -414,9 +422,9 @@ $this->on('system-schema-search-elastic', function ($request, $response) {
     //----------------------------//
     // 4. Process Data
     $path = $this->package('global')->path('config') . '/schema/elastic/';
-    
+
     $files = @scandir($path);
-    
+
     if (!$files) {
         return $response->setError(true, 'No elastic schemas found.');
     }
@@ -425,7 +433,7 @@ $this->on('system-schema-search-elastic', function ($request, $response) {
     if (isset($data['filter']['active'])) {
         $active = $data['filter']['active'];
     }
-    
+
     $results = [];
     foreach ($files as $file) {
         if (trim($file) == '.'
@@ -435,16 +443,16 @@ $this->on('system-schema-search-elastic', function ($request, $response) {
         }
 
         if (file_exists(sprintf($path . '/%s/elastic.php', $file))) {
-            
+
             $model = $this->package('global')
                 ->config('admin/schema/%s.php', strtolower($file));
 
             $model['name'] = $model['singular'] = strtolower($file);
             $results[] = $model;
         }
-        
+
     }
-    
+
     //set response format
     $response->setError(false)->setResults([
         'rows' => $results,
@@ -459,7 +467,7 @@ $this->on('system-schema-search-elastic', function ($request, $response) {
  * @param Response $response
  */
 $cradle->on('system-schema-map-elastic', function ($request, $response) {
-    
+
     //----------------------------//
     // 1. Get Data
     $data = [];
@@ -476,7 +484,7 @@ $cradle->on('system-schema-map-elastic', function ($request, $response) {
     $modelPath = sprintf($this->package('global')
             ->path('config') . '/schema/%s.php',
         $data['name']);
-        
+
     if (!file_exists($modelPath)) {
         return $response->setError(true, 'Model doesn\'t exist.');
     }
@@ -484,23 +492,23 @@ $cradle->on('system-schema-map-elastic', function ($request, $response) {
     //----------------------------//
     // 3. Prepare Data
     //----------------------------//
-    
+
     //----------------------------//
     // 4. Process Data
     //----------------------------//
     $schema = Schema::i($data['name']);
-    
+
     //map elastic
-    
+
 
     //create elastic mappings
     $map = $schema->service('elastic')->map();
-    
+
     //check if mapping is successfull
     if (!$map) {
         return $response->setError(true, 'Something went wrong.');
     }
-    
+
     $response->setError(false);
 });
 
@@ -510,7 +518,7 @@ $cradle->on('system-schema-map-elastic', function ($request, $response) {
  * @param Request $request
  * @param Response $response
  */
-$cradle->on('system-schema-populate-elastic', function($request, $response) {   
+$cradle->on('system-schema-populate-elastic', function($request, $response) {
     //----------------------------//
     // 1. Get Data
     $data = [];
@@ -527,7 +535,7 @@ $cradle->on('system-schema-populate-elastic', function($request, $response) {
     $modelPath = sprintf($this->package('global')
             ->path('config') . '/schema/%s.php',
         $data['name']);
-        
+
     if (!file_exists($modelPath)) {
         return $response->setError(true, 'Model doesn\'t exist.');
     }
@@ -535,7 +543,7 @@ $cradle->on('system-schema-populate-elastic', function($request, $response) {
     //----------------------------//
     // 3. Prepare Data
     //----------------------------//
-    
+
     //----------------------------//
     // 4. Process Data
     //----------------------------//
@@ -545,7 +553,7 @@ $cradle->on('system-schema-populate-elastic', function($request, $response) {
     if (!$populate) {
         return $response->setError(true, 'Something went wrong during elastic poulate');
     }
-    
+
     $response->setError(false);
 });
 
@@ -555,7 +563,7 @@ $cradle->on('system-schema-populate-elastic', function($request, $response) {
  * @param Request $request
  * @param Response $response
  */
-$cradle->on('system-schema-flush-elastic', function($request, $response) { 
+$cradle->on('system-schema-flush-elastic', function($request, $response) {
     //----------------------------//
     // 1. Get Data
     $data = [];
@@ -572,15 +580,15 @@ $cradle->on('system-schema-flush-elastic', function($request, $response) {
     $modelPath = sprintf($this->package('global')
             ->path('config') . '/schema/%s.php',
         $data['name']);
-        
+
     if (!file_exists($modelPath)) {
         return $response->setError(true, 'Model doesn\'t exist.');
     }
-    
+
     //----------------------------//
     // 3. Prepare Data
     //----------------------------//
-    
+
     //----------------------------//
     // 4. Process Data
     //----------------------------//
@@ -591,7 +599,7 @@ $cradle->on('system-schema-flush-elastic', function($request, $response) {
     if (!$flush) {
         return $response->setError(true, 'Something went wrong.');
     }
-    
+
     return $response->setError(false);
 });
 
@@ -601,7 +609,7 @@ $cradle->on('system-schema-flush-elastic', function($request, $response) {
  * @param Request $request
  * @param Response $response
  */
-$cradle->on('system-schema-get-elastic', function($request, $response) { 
+$cradle->on('system-schema-get-elastic', function($request, $response) {
     //----------------------------//
     // 1. Get Data
     $data = [];
@@ -618,15 +626,15 @@ $cradle->on('system-schema-get-elastic', function($request, $response) {
     $modelPath = sprintf($this->package('global')
             ->path('config') . '/schema/elastic/%s/elastic.php',
         ucwords($data['name']));
-        
+
     if (!file_exists($modelPath)) {
         return $response->setError(true, 'Schema doesn\'t exist.');
     }
-    
+
     //----------------------------//
     // 3. Prepare Data
     //----------------------------//
-    
+
     //----------------------------//
     // 4. Process Data
     //----------------------------//
@@ -677,6 +685,6 @@ $cradle->on('system-schema-update-elastic', function($request, $response) {
     } catch (\Throwable $e) {
         return $response->setError(true, $e->getMessage());
     }
-    
+
     return $response->setError(false);
 });
