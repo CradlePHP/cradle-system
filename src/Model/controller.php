b06e8d9386d4372c6576a065b0a8e3b4c0807094
aprilsacil <aprilvsacil@gmail.com>
2018-06-20 15:35:23 +0800

    updated webhook uris on model

--- src/Model/controller.php
+++ src/Model/controller.php
@@ -124,8 +124,9 @@ $this->get('/admin/system/model/:schema/search', function ($request, $response) 
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/' . $request->getStage('schema') . '/search';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/search',
+            'uri' => $uri,
             'method' => 'get',
             'json_data' => json_encode($data)
         ];
@@ -277,8 +278,9 @@ $this->get('/admin/system/model/:schema/create', function ($request, $response) 
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/' . $request->getStage('schema') . '/create';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/create',
+            'uri' => $uri,
             'method' => 'get',
             'json_data' => json_encode($data)
         ];
@@ -469,8 +471,9 @@ $this->get('/admin/system/model/:schema/update/:id', function ($request, $respon
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/'.$request->getStage('schema').'/update/:id';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/update/:id',
+            'uri' => $uri,
             'method' => 'get',
             'json_data' => json_encode($data)
         ];
@@ -660,8 +663,9 @@ $this->get('/admin/system/model/:schema/detail/:id', function ($request, $respon
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/'.$request->getStage('schema').'/detail/:id';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/detail/:id',
+            'uri' => $uri,
             'method' => 'get',
             'json_data' => json_encode($data)
         ];
@@ -948,8 +952,9 @@ $this->post('/admin/system/model/:schema/create', function ($request, $response)
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/'.$request->getStage('schema').'/create';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/create',
+            'uri' => $uri,
             'method' => 'post',
             'json_data' => json_encode($response->getResults())
         ];
@@ -1090,8 +1095,9 @@ $this->post('/admin/system/model/:schema/update/:id', function ($request, $respo
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/'.$request->getStage('schema').'/update/:id';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/update/:id',
+            'uri' => $uri,
             'method' => 'post',
             'json_data' => json_encode($response->getResults())
         ];
@@ -1167,8 +1173,9 @@ $this->get('/admin/system/model/:schema/remove/:id', function ($request, $respon
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/'.$request->getStage('schema').'/remove/:id';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/remove/:id',
+            'uri' => $uri,
             'method' => 'get',
             'json_data' => json_encode($response->getResults())
         ];
@@ -1247,8 +1254,9 @@ $this->get('/admin/system/model/:schema/restore/:id', function ($request, $respo
 
     // execute webhook distribution
     try {
+        $uri = '/admin/system/model/'.$request->getStage('schema').'/restore/:id';
         $webhook = [
-            'uri' => '/admin/system/model/:schema/restore/:id',
+            'uri' => $uri,
             'method' => 'get',
             'json_data' => json_encode($response->getResults())
         ];
@@ -1361,8 +1369,9 @@ $this->post('/admin/system/model/:schema/import', function ($request, $response)
     if ($request->getStage('render') != 'false') {
         // execute webhook distribution
         try {
+            $uri = '/admin/system/model/'.$request->getStage('schema').'/import';
             $webhook = [
-                'uri' => '/admin/system/model/:schema/import',
+                'uri' => $uri,
                 'method' => 'post',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -1787,271 +1796,6 @@ $this->get('/admin/system/model/:schema/calendar', function ($request, $response
     $this->trigger('admin-render-page', $request, $response);
 });
 
-// /**
-//  * Render the System Model Pipeline Page
-//  *
-//  * @param Request $request
-//  * @param Response $response
-//  */
-// $this->get('/admin/system/model/:schema/pipeline', function ($request, $response) {
-//     //----------------------------//
-//     // 1. Prepare Data
-//     $data = $request->getStage();
-//
-//     // set redirect
-//     $redirect = sprintf(
-//         '/admin/system/model/%s/search',
-//         $request->getStage('schema')
-//     );
-//
-//     if ($request->getStage('redirect_uri')) {
-//         $redirect = $request->getStage('redirect_uri');
-//     }
-//
-//     $request->setStage('redirect_uri', $redirect);
-//
-//     if (!isset($data['ajax'])) {
-//         $data['ajax'] = [];
-//     }
-//
-//     // if no ajax set, set this page as default
-//     if (!isset($data['ajax']['pull']) || empty($data['ajax']['pull'])) {
-//         $data['ajax']['pull'] = sprintf(
-//             '/admin/system/model/%s/search',
-//             $request->getStage('schema')
-//         );
-//     }
-//
-//     if (!isset($data['ajax']['update']) || empty($data['ajax']['update'])) {
-//         $data['ajax']['update'] = sprintf(
-//             '/admin/system/model/%s/pipeline',
-//             $request->getStage('schema')
-//         );
-//     }
-//
-//     // if no detail set, set update page as the default
-//     if (!isset($data['detail']) || empty($data['detail'])) {
-//         $data['detail'] = sprintf(
-//             '/admin/system/model/%s/update',
-//             $request->getStage('schema')
-//         );
-//     }
-//
-//     //----------------------------//
-//     // 2. Validate
-//     // does the schema exists?
-//     try {
-//         $data['schema'] = Schema::i($request->getStage('schema'))->getAll();
-//     } catch (\Exception $e) {
-//         $message = $this
-//             ->package('global')
-//             ->translate($e->getMessage());
-//
-//         $response->setError(true, $message);
-//     }
-//
-//     //if this is a return back from processing
-//     //this form and it's because of an error
-//     if ($response->isError()) {
-//         //pass the error messages to the template
-//         $this
-//             ->package('global')
-//             ->flash($response->getMessage(), 'error');
-//         $this
-//             ->package('global')
-//             ->redirect($redirect);
-//     }
-//
-//     //check what to show
-//     if (!isset($data['show']) || !$data['show']) {
-//         //flash an error message and redirect
-//         $error = $this
-//             ->package('global')
-//             ->translate('Please specify what to plot.');
-//         $this
-//             ->package('global')
-//             ->flash($error, 'error');
-//         $this
-//             ->package('global')
-//             ->redirect($redirect);
-//     }
-//
-//     // minimize long array chain
-//     $fields = $data['schema']['fields'];
-//     // pipeline stages
-//     $data['stages'] = $fields[$data['show']]['field']['options'];
-//
-//     $allowed = ['select', 'radios'];
-//
-//     if (!isset($fields[$data['show']])
-//         || !in_array($fields[$data['show']]['field']['type'], $allowed)
-//     ) {
-//         $error = $this
-//             ->package('global')
-//             ->translate('%s is not a select/radio field', $show);
-//         $this
-//             ->package('global')
-//             ->flash($error, 'error');
-//         $this
-//             ->package('global')
-//             ->redirect($redirect);
-//     }
-//
-//     $dates = ['date', 'datetime', 'created', 'updated', 'time', 'week', 'month'];
-//     if (isset($data['date'])
-//         && (!isset($fields[$data['date']])
-//             || !in_array($fields[$data['date']]['field']['type'], $dates))
-//     ) {
-//         $error = $this
-//             ->package('global')
-//             ->translate('%s is not a type of date field', $data['date']);
-//         $this
-//             ->package('global')
-//             ->flash($error, 'error');
-//         $this
-//             ->package('global')
-//             ->redirect($redirect);
-//     }
-//
-//     // initialize the stageHeader keys into null
-//     // so that even there's no total or range, it will not get errors
-//     $data['stageHeader'] = [
-//         'total' => null,
-//         'minRange' => null,
-//         'maxRange' => null
-//     ];
-//
-//     // check if the user want's to display the total
-//     if (isset($data['total'])) {
-//         if (strpos($data['total'], ',') !== false) {
-//             $data['total'] = explode(',', $data['total']);
-//         }
-//
-//         // if not array, make it an array
-//         if (!is_array($data['total'])) {
-//             $data['total'] = [$data['total']];
-//         }
-//
-//         $rangeFieldTypes = ['number', 'small', 'float', 'price'];
-//
-//         $fields = $data['schema']['fields'];
-//         foreach ($data['total'] as $field) {
-//             if (isset($fields[$field])
-//                 && in_array($fields[$field]['field']['type'], $rangeFieldTypes)
-//             ) {
-//                 continue;
-//             }
-//
-//             // flash error message
-//             $error = $this
-//                 ->package('global')
-//                 ->translate('%s is not a number type field', $value);
-//
-//             $this
-//                 ->package('global')
-//                 ->flash($error, 'error');
-//
-//             $this
-//                 ->package('global')
-//                 ->redirect($redirect);
-//         }
-//     }
-//
-//     $data['schema']['filterable'] = array_values($data['schema']['filterable']);
-//     //----------------------------//
-//     // 3. Render Template
-//     $data['title'] = $this
-//         ->package('global')
-//         ->translate('%s Pipeline', $data['schema']['singular']);
-//
-//     $class = sprintf('page-admin-%s-pipeline page-admin', $request->getStage('schema'));
-//     $body = $this
-//         ->package('cradlephp/cradle-system')
-//         ->template('model', 'board', $data, ['search_form', 'search_filters']);
-//
-//     //Set Content
-//     $response
-//         ->setPage('title', $data['title'])
-//         ->setPage('class', $class)
-//         ->setContent($body);
-//
-//     //Render blank page
-//     $this->trigger('admin-render-page', $request, $response);
-// });
-//
-// /**
-//  * Process Pipeline Update
-//  *
-//  * @param Request $request
-//  * @param Response $response
-//  */
-// $this->post('/admin/system/model/:schema/pipeline', function ($request, $response) {
-//     //----------------------------//
-//     // get json response data only
-//     $request->setStage('redirect_uri', 'false');
-//     $request->setStage('render', 'false');
-//
-//     $data = [];
-//     if ($request->getStage()) {
-//         $data = $request->getStage();
-//     }
-//
-//     $filters = [];
-//
-//     // if it reached here, then we're assumming
-//     // that the user attempted to do an order/sort update
-//     if (isset($data['moved']) && isset($data['sort'])) {
-//         if (isset($data['stage']) && isset($data[$data['stage']])) {
-//             $filters[] = sprintf(
-//                 '%s = "%s"',
-//                 $data['stage'],
-//                 $data[$data['stage']]
-//             );
-//         }
-//
-//         // if it was moved upwards
-//         // then we only update the rows from this item to the previous elder
-//         if ($request->getStage('moved') == 'upwards') {
-//             $request->setStage(
-//                 'fields',
-//                 [$data['sort'] => $data['sort'] . '+1']
-//             );
-//
-//             $filters = [
-//                 $data['sort'] . ' > ' . $data['new_elder'],
-//                 $data['sort'] . ' <= ' . $data['previous_elder']
-//             ];
-//         }
-//
-//         // if it was moved downwards
-//         // then we update from the previous elder to the newest elder
-//         if ($request->getStage('moved') == 'downwards') {
-//             $request->setStage(
-//                 'fields',
-//                 [$data['sort'] => $data['sort'] . '-1']
-//             );
-//             $filters = [
-//                 $data['sort'] . ' >= ' . $data['previous_elder'],
-//                 $data['sort'] . ' <= ' . $data['new_elder']
-//             ];
-//
-//         }
-//
-//         $request->setStage('filters', $filters);
-//     }
-//
-//     return $this->routeTo(
-//         'post',
-//         sprintf(
-//             '/admin/system/model/%s/update/%s',
-//             $request->getStage('schema'),
-//             $request->getStage('id')
-//         ),
-//         $request,
-//         $response
-//     );
-// });
-
 //Front End Controllers
 
 /**
@@ -2079,8 +1823,9 @@ $this->get('/system/model/:schema/search', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/search';
             $webhook = [
-                'uri' => '/system/model/:schema/search',
+                'uri' => $uri,
                 'method' => 'get',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2118,8 +1863,9 @@ $this->get('/system/model/:schema/create', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/create';
             $webhook = [
-                'uri' => '/system/model/:schema/create',
+                'uri' => $uri,
                 'method' => 'get',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2158,8 +1904,9 @@ $this->get('/system/model/:schema/update/:id', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/update/:id';
             $webhook = [
-                'uri' => '/system/model/:schema/update/:id',
+                'uri' => $uri,
                 'method' => 'get',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2220,8 +1967,9 @@ $this->post('/system/model/:schema/create', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/create';
             $webhook = [
-                'uri' => '/system/model/:schema/create',
+                'uri' => $uri,
                 'method' => 'post',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2260,8 +2008,9 @@ $this->post('/system/model/:schema/update/:id', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/update/:id';
             $webhook = [
-                'uri' => '/system/model/:schema/update/:id',
+                'uri' => $uri,
                 'method' => 'post',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2300,8 +2049,9 @@ $this->get('/system/model/:schema/remove/:id', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/remove/:id';
             $webhook = [
-                'uri' => '/system/model/:schema/remove/:id',
+                'uri' => $uri,
                 'method' => 'get',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2340,8 +2090,9 @@ $this->get('/system/model/:schema/restore/:id', function ($request, $response) {
     // if successful, execute webhook distribution
     if (!$response->isError()) {
         try {
+            $uri = '/system/model/'.$request->getStage('schema').'/restore/:id';
             $webhook = [
-                'uri' => '/system/model/:schema/restore/:id',
+                'uri' => $uri,
                 'method' => 'get',
                 'json_data' => json_encode($response->getResults())
             ];
@@ -2376,8 +2127,9 @@ $this->post('/system/model/:schema/import', function ($request, $response) {
 
     // execute webhook distribution
     try {
+        $uri = '/system/model/'.$request->getStage('schema').'/import';
         $webhook = [
-            'uri' => '/system/model/:schema/import',
+            'uri' => $uri,
             'method' => 'post',
             'json_data' => json_encode($response->getResults())
         ];
