e3a54159883bb2c30af8f6c964527ca495fc1957
Christian Blanquera <cblanquera@openovate.com>
2019-01-15 01:42:14 +0800

    removing controllers loading from bootstrap in favor of route and event priorities. Upping version to 2.2 matching the framework version

--- .cradle.php
+++ .cradle.php
@@ -6,9 +6,13 @@
  * distributed with this package.
  */
 require_once __DIR__ . '/src/Schema/events.php';
-require_once __DIR__ . '/src/Relation/events.php';
+require_once __DIR__ . '/src/Schema/controller.php';
 require_once __DIR__ . '/src/Model/events.php';
+require_once __DIR__ . '/src/Model/controller.php';
 require_once __DIR__ . '/src/Fieldset/events.php';
+require_once __DIR__ . '/src/Fieldset/controller.php';
+require_once __DIR__ . '/src/Relation/events.php';
+require_once __DIR__ . '/src/Relation/controller.php';
 
 //bootstrap
 $this
@@ -16,5 +20,4 @@ $this
     ->preprocess(include __DIR__ . '/src/bootstrap/helpers.php')
     ->preprocess(include __DIR__ . '/src/bootstrap/template.php')
     ->preprocess(include __DIR__ . '/src/bootstrap/files.php')
-    ->preprocess(include __DIR__ . '/src/bootstrap/fieldset.php')
-    ->preprocess(include __DIR__ . '/src/bootstrap/controller.php');
+    ->preprocess(include __DIR__ . '/src/bootstrap/fieldset.php');
