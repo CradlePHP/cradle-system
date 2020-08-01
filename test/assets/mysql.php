f07a1a318b4538d91b9518af5351214dbd024302
Christian Blanquera <cblanquera@openovate.com>
2020-08-01 09:15:24 +0800

    updated tests to work independently

--- test/assets/mysql.php
+++
@@ -1,3 +0,0 @@
-<?php //-->
-
-return new PDO('mysql:host=127.0.0.1;dbname=testing_db', 'root', '');
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,19 +6,8 @@
  * Copyright and license information can be found at LICENSE
  * distributed with this package.
  */
-
-if (!defined('CRADLE_CWD')) {
-  if(file_exists(__DIR__ . '/../../../.cradle.php')) {
-    require_once __DIR__ . '/../../../.cradle.php';
-  }
-
-  if(file_exists(__DIR__ . '/../../../../.cradle.php')) {
-    require_once __DIR__ . '/../../../../.cradle.php';
-  }
+if(file_exists(__DIR__.'/../../../../vendor/autoload.php')) {
+  require_once __DIR__.'/../../../../vendor/autoload.php';
+} else {
+  require_once __DIR__.'/../vendor/autoload.php';
 }
-
-if (!defined('CRADLE_CWD')) {
-  throw new Exception('Cannot find root of project');
-}
-
-cradle()->prepare();
